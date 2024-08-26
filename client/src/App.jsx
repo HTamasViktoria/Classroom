e0dd9a347156be613af7800d4a93c8a6e1730757
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-26 08:22:25 +0200

    🚧 backend and frontend blank project created

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {AppBar} from '@mui/material'
+import './App.css'
+import {IconButton, Toolbar, Typography, Box} from "@mui/material";
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+
+function App() {
+    return (
+       <></>
+    )
+}
+
+export default App
