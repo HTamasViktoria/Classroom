1797989c4e8832a0601f9670d8f9be77ea74fe04
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-28 09:20:35 +0200

    🚧 added GradeValues.cs and Subjects.cs enum classes

---
+++ Classroom/Model/Enums/Subjects.cs
@@ -0,0 +1,13 @@
+namespace Classroom.Model.DataModels.Enums;
+
+public enum Subjects
+{
+    Nyelvtan,
+    Irodalom,
+    Matematika,
+    Környezetismeret,
+    Angol,
+    Ének,
+    Rajz,
+    Testnevelés
+}
