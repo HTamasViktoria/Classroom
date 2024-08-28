b2f407b60282000a07e27791c152d37aa1a18241
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-28 09:25:01 +0200

    🚧 Program.cs updated

--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -1,6 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text;
+//using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Classroom.Data;
+using Classroom.Service.Repositories;
+using Microsoft.OpenApi.Models;
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Hello World!");
+        ConfigureServices(builder.Services, builder.Configuration);
+        ConfigureSwagger(builder.Services);
+        ConfigureDatabaseContexts(builder.Services, builder.Configuration);
+        ConfigureCustomServices(builder.Services);
 
-app.Run();
+        var app = builder.Build();
+        ConfigureMiddleware(app);
+        
+        app.Run();
+    }
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+    }
+
+    private static void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        });
+    }
+
+    private static void ConfigureDatabaseContexts(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MSSQL_CONNECTION");
+        
+        services.AddDbContext<ClassroomContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+   
+    }
+
+    private static void ConfigureCustomServices(IServiceCollection services)
+    {
+        services.AddScoped<ITeacherRepository, TeacherRepository>();
+        services.AddScoped<IGradeRepository, GradeRepository>();
+        services.AddScoped<IStudentRepository, StudentRepository>();
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+    }
+}
