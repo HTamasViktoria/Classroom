c1c1a8d4f110dc5be33363177d5b0221dfd70102
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-29 22:08:06 +0200

    🚧 main.jsx updated(Added new routes and components for managing students and teachers)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';  // A helyes importálás React 18 esetén
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  // react-router-dom importálása
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import Students from "./admin/Students.jsx";
+import Teachers from "./admin/Teachers.jsx";
 import ParentStarter from "./parent/Starter.jsx";
-import AdminStarter from "./admin/Starter.jsx"
-import TeacherStarter from "./teacher/Starter.jsx"
+import AdminStarter from "./admin/Starter.jsx";
+import TeacherStarter from "./teacher/Starter.jsx";
+import TeacherAdding from "./admin/TeacherAdding.jsx";
+import StudentAdding from "./admin/StudentAdding.jsx";
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +22,12 @@ createRoot(document.getElementById('root')).render(
           <Route path='/parent' element={<ParentStarter/>}/>
           <Route path='/admin' element={<AdminStarter/>}/>
           <Route path='/teacher' element={<TeacherStarter/>}/>
+          <Route path='/students' element={<Students/>}/>
+          <Route path='/teachers' element={<Teachers/>}/>
+          <Route path='/add-teacher' element={<TeacherAdding/>}/>
+          <Route path='/add-student' element={<StudentAdding/>}/>
+          
+          
       </Routes>
   </BrowserRouter>
   </StrictMode>,
