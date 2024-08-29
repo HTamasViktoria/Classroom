bedd7cd600a76c5a5759957f7336b4ec8d3f02e2
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-29 21:32:00 +0200

    🚧 Teachers.jsx added to admin-pages

---
+++ client/src/admin/Teachers.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import Button from '@mui/material/Button';
+import AdminTeacherList from "../components/AdminTeacherList.jsx";
+import AdminNavbar from "../components/AdminNavbar.jsx";
+
+function Teachers(){
+    const navigate = useNavigate();
+
+    const [allTeachers, setAllTeachers] = useState([]);
+
+    useEffect(() => {
+
+        fetch('/api/teachers')
+            .then(response => response.json())
+            .then(data => setAllTeachers(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+    
+    return(<>
+        <AdminNavbar/>
+        <Button
+            variant="contained"
+            sx={{
+                backgroundColor: '#b5a58d',
+                color: '#fff',
+                '&:hover': {
+                    backgroundColor: '#b5a58d',
+                },
+            }}
+            onClick={() => navigate("/add-teacher")}
+        >
+            Tanár hozzáadása
+        </Button>
+        
+        <AdminTeacherList teachers={allTeachers}/>
+    </>)
+}
+
+export default Teachers
