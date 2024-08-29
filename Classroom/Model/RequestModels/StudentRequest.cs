17b36c8e6ac96b31333f194c32f541ec566fb07c
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-08-29 21:39:48 +0200

    🚧 StudentRequest.cs request-model class added to the backend

--- Classroom/Model/RequestModels/StudentRequest.cs
+++ Classroom/Model/RequestModels/StudentRequest.cs
@@ -6,10 +6,10 @@ public class StudentRequest
 {
     public string FirstName { get; set; }
     public string FamilyName { get; set; }
-    public DateTime BirthDate { get; init; }
+    public string BirthDate { get; init; }
     public string BirthPlace { get; init; }
     public string StudentNo { get; init; }
-    public ICollection<GradeValues> Grades { get; init; } = new List<GradeValues>();
+    
 
 
 }
