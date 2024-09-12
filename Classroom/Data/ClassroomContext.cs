2751e6a5958382663f3b4ee10a45cdb0ecfef2b6
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-12 20:06:20 +0200

    :construction: chore: Notification entity added to db

--- Classroom/Data/ClassroomContext.cs
+++ Classroom/Data/ClassroomContext.cs
@@ -7,10 +7,23 @@ public class ClassroomContext : DbContext
 {
     public DbSet<Teacher> Teachers { get; set; }
     public DbSet<Student> Students { get; set; }
-    public DbSet<Grade> Grades { get; set; }
-
+    public DbSet<Grade> Grades { get; set; }   
+    public DbSet<NotificationBase> Notifications { get; set; }
+  
+    
     public ClassroomContext(DbContextOptions<ClassroomContext> options)
         : base(options)
     {
     }
+    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NotificationBase>()
+            .HasMany(n => n.Students)
+            .WithMany(s => s.Notifications)
+            .UsingEntity(j => j.ToTable("NotificationStudents"));
+    }
+
+   
 }
