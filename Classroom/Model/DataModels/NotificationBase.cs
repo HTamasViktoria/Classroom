3beb3f507519d398f3efb1ab7a4ec1a883f7b0a7
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-12 19:26:42 +0200

    🚧 chore: NotificationBase.cs added to DataModels

---
+++ Classroom/Model/DataModels/NotificationBase.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Classroom.Model.DataModels.Enums;
+
+namespace Classroom.Model.DataModels
+{
+    public class NotificationBase
+    {
+        [Key]
+        public int Id { get; init; }
+
+        [ForeignKey("TeacherId")]
+        public int TeacherId { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        
+        public List<Student> Students { get; set; } = new List<Student>();
+    
+        public string Description { get; set; }
+        public Subjects? Subject { get; set; }
+        public string? OptionalDescription { get; set; }
+    }
+}
