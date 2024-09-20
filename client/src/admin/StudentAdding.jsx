3d8bb2ea6ec2f732d4271c875d944a5a45e867bb
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-20 10:31:41 +0200

    :adhesive_bandage: place of StudentAddingForm.jsx corrected in StudentAdding.jsx(StudentAddingForm moved to admin components

--- client/src/admin/StudentAdding.jsx
+++ client/src/admin/StudentAdding.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import AdminNavbar from "../components/Admin/AdminNavbar.jsx";
-import StudentAddingForm from "../components/Teacher/StudentAddingForm.jsx";
+import StudentAddingForm from "../components/Admin/StudentAddingForm.jsx";
 function StudentAdding(){
 
     const navigate = useNavigate();
