085285a8b411707f90cb7cea3b53cc2596620fe9
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-20 13:54:39 +0200

    🩹 fix: new properties added to NotificationRequest.cs

--- Classroom/Model/RequestModels/NotificationRequest.cs
+++ Classroom/Model/RequestModels/NotificationRequest.cs
@@ -8,8 +8,10 @@ public class NotificationRequest
    
         public string Type { get; set; }
         public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
         public DateTime Date { get; set; }
         public string? Subject { get; set; }
+        public bool Read { get; set; }
         public string Description { get; set; }
         public List<int> StudentIds { get; set; }
         public string? OptionalDescription { get; set; }
