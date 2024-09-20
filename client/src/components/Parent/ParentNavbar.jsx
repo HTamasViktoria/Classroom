14ea87b7545d6118b50bd638365bae64a1e1efbf
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-20 11:40:00 +0200

    🚧 chore: ParentNavbar shows with badge the number of all notifications

--- client/src/components/Parent/ParentNavbar.jsx
+++ client/src/components/Parent/ParentNavbar.jsx
@@ -1,13 +1,22 @@
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import React from 'react';
+import { AppBar, Toolbar, Typography, Box, Badge } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import React, { useState, useEffect } from 'react';
 
 function ParentNavbar(props) {
+    const [notifications, setNotifications] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/notifications/byStudentId/${props.student.id}`)
+            .then(response => response.json())
+            .then(data => setNotifications(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, [props.student]);
 
     const clickHandler = (event) => {
         const chosen = event.target.getAttribute('data-value');
         console.log(chosen);
         props.onChosen(chosen);
-    }
+    };
 
     return (
         <AppBar sx={{ backgroundColor: '#d2a679' }}>
@@ -19,9 +28,13 @@ function ParentNavbar(props) {
                     <Typography onClick={clickHandler} component='div' data-value="grades">
                         Osztályzatok
                     </Typography>
-                    <Typography onClick={clickHandler} component='div' data-value="notifications">
+                    <Typography>
                         Értesítések
+                        <span style={{ marginLeft: '8px', backgroundColor: '#f44336', color: 'white', borderRadius: '12px', padding: '4px 8px' }}>
+        {notifications.length}
+    </span>
                     </Typography>
+
                     <Typography onClick={clickHandler} component='div' data-value="absences">
                         Hiányzások
                     </Typography>
