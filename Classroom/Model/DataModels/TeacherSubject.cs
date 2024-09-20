7ca3cb89bdac16efd134eb40602ce4d158095152
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-20 10:36:35 +0200

    🚧 chore: TeacherSubject.cs added to model classes

---
+++ Classroom/Model/DataModels/TeacherSubject.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Classroom.Model.DataModels.Enums;
+
+namespace Classroom.Model.DataModels
+{
+    public class TeacherSubject
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+        
+        [ForeignKey(nameof(TeacherId))]
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+        
+        [ForeignKey(nameof(ClassOfStudentsId))]
+        public int ClassOfStudentsId { get; set; }
+        public ClassOfStudents ClassOfStudents { get; set; }
+    }
+}
