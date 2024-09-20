d4da5599612847de4856ffb40b3fd0f01af80b2d
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-20 10:58:21 +0200

    🚧 chore: new routes added to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,25 +10,38 @@ import AdminStarter from "./admin/Starter.jsx";
 import TeacherStarter from "./teacher/Starter.jsx";
 import TeacherAdding from "./admin/TeacherAdding.jsx";
 import StudentAdding from "./admin/StudentAdding.jsx";
+import Classes from "./admin/Classes.jsx";
+import ClassAdding from "./admin/ClassAdding.jsx";
+import Teacher from "./admin/Teacher.jsx";
 
 
 
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <BrowserRouter>
-      <Routes>
-          <Route path='/parent/:id' element={<ParentStarter />} />
-          <Route path='/admin' element={<AdminStarter/>}/>
-          <Route path='/teacher' element={<TeacherStarter/>}/>
-          <Route path='/students' element={<Students/>}/>
-          <Route path='/teachers' element={<Teachers/>}/>
-          <Route path='/add-teacher' element={<TeacherAdding/>}/>
-          <Route path='/add-student' element={<StudentAdding/>}/>
-          
-          
-      </Routes>
-  </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/parent/:id' element={<ParentStarter />} />
+                <Route path='/admin' element={<AdminStarter/>}/>
+                <Route path='/teacher' element={<TeacherStarter/>}/>
+                <Route path='/students' element={<Students/>}/>
+                <Route path='/teachers' element={<Teachers/>}/>
+                <Route path='/add-teacher' element={<TeacherAdding/>}/>
+                <Route path='/add-student' element={<StudentAdding/>}/>
+                <Route path='/classes' element={<Classes/>}/>
+                <Route path='/add-class' element={<ClassAdding/>}/>
+                <Route path='/teachers/:id' element={<Teacher />} />
+               
+
+
+
+
+
+
+
+
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
