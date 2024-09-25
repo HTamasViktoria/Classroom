335634f3b855e5644c1ecdb90841f1140a8420cf
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-25 14:33:54 +0200

    🩹 fix: Classes.jsx corrected

--- client/src/admin/Classes.jsx
+++ client/src/admin/Classes.jsx
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from "react";
-import MainClasses from "../components/Admin/MainClasses.jsx";
-import StudentsOfClass from "../components/Admin/StudentsOfClass.jsx";
+import AdminClassList from "../components/Admin/AdminClassList.jsx";
+import AdminNavbar from "../components/Admin/AdminNavbar.jsx";
+import Button from "@mui/material/Button";
 import StudentAddingToClass from "../components/Admin/StudentAddingToClass.jsx";
+import {useNavigate} from "react-router-dom";
+import StudentsOfClass from "../components/Admin/StudentsOfClass.jsx";
 
 
 function Classes() {
-   
-    
+
+    const navigate = useNavigate();
+
     const [classes, setClasses] = useState([]);
     const [classId, setClassId] = useState(null)
     const [className, setClassName] = useState("")
@@ -19,21 +23,82 @@ function Classes() {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    
-const handleViewStudents = (classId, className) =>{
-    setClassId(classId)
-    setClassName(className)
-    setAddingOrViewing("viewing")
-}
 
-const handleAddStudent =(classId, className) =>{
-    setClassName(className);
-    setClassId(classId)
-    setAddingOrViewing("adding")
-}
+    const handleViewStudents = (classId, className) => {
+        setClassId(classId)
+        setClassName(className)
+        setAddingOrViewing("viewing")
+    }
+
+    const handleAddStudent = (classId, className) => {
+        setClassName(className);
+        setClassId(classId)
+        setAddingOrViewing("adding")
+    }
+
+    const goBackFromAddingHandler=()=>{
+        setAddingOrViewing("")
+    }
+
+    const goBackFromViewingHandler=()=>{
+        setAddingOrViewing("")
+    }
+    
+    const goBackHandler = () => {
+        navigate("/admin")
+    }
 
     return (
         <>
+            <AdminNavbar/>
+
+            {addingOrViewing === "adding" ? (
+                <><StudentAddingToClass classId={classId} className={className}/>
+                    <Button variant="contained"
+                            sx={{
+                                backgroundColor: '#c7b19f',
+                                color: '#fff',
+                                '&:hover': {
+                                    backgroundColor: '#b29f8f',
+                                },
+                                marginTop: 2,
+                            }}
+                            onClick={goBackFromAddingHandler}>
+                        Vissza
+                    </Button></>
+            ) : addingOrViewing === "viewing" ? (
+                <StudentsOfClass classId={classId} className={className} onGoBack={goBackFromViewingHandler}/>
+            ) : (<>
+                <AdminClassList
+                    classes={classes}
+                    onViewStudents={handleViewStudents}
+                    onAddStudent={handleAddStudent}
+                /><Button variant="contained"
+                          sx={{
+                              backgroundColor: '#c7b19f',
+                              color: '#fff',
+                              '&:hover': {
+                                  backgroundColor: '#b29f8f',
+                              },
+                              marginTop: 2,
+                          }}
+                          onClick={goBackHandler}>
+                    Vissza
+                </Button>
+                    </>
+            )}
+           {/* */}
+
+        </>
+    );
+}
+
+export default Classes;
+
+
+/*
+* <>
+        <AdminNavbar/>
             {addingOrViewing === "adding" ? (
                 <StudentAddingToClass classId={classId} className={className} />
             ) : addingOrViewing === "viewing"? (
@@ -46,7 +111,4 @@ const handleAddStudent =(classId, className) =>{
                 />
             )}
         </>
-    );
-}
-
-export default Classes;
+* */
