3b4a2ac9309d178e7f792ff1ac8f2d1e3ce432e9
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-26 20:56:39 +0200

    :recycle: refactor: NotificationAdder.jsx refactored, validateForm.js and buttonText.js are separated from it

--- client/src/components/Teacher/NotificationAdder.jsx
+++ client/src/components/Teacher/NotificationAdder.jsx
@@ -5,113 +5,126 @@ import DateSelector from "./DateSelector.jsx";
 import ChooseFromStudentsSelector from "./ChooseFromStudentsSelector.jsx";
 import DescriptionSelector from "./DescriptionSelector.jsx";
 import OptionalDescriptionSelector from "./OptionalDescriptionSelector.jsx";
+import ButtonText from "./buttonText.js";
+import validateForm from "./validateForm.js";
 
 function NotificationAdder(props) {
-    const [selectedDate, setSelectedDate] = useState("");
-    const [selectedSubjectId, setSelectedSubjectId] = useState("")
-    const [selectedSubjectName, setSelectedSubjectName] = useState("")
-    const [description, setDescription] = useState("");
-    const [optionalDescription, setOptionalDescription] = useState("");
-    const [allStudents, setAllStudents] = useState([])
-    const [students, setStudents] = useState([]);
-    const [alertMessage, setAlertMessage] = useState("");
+    const [state, setState] = useState({
+        selectedDate: "",
+        selectedSubjectId: "",
+        selectedSubjectName: "",
+        description: "",
+        optionalDescription: "",
+        allStudents: [],
+        students: [],
+        alertMessage: "",
+    });
 
-    const dateChangeHandler = (date) => setSelectedDate(date);
-    const textChangeHandler = (text) => setDescription(text);
-    const selectedStudentsHandler = (students) => setStudents(students);
-    const descriptionHandler = (description) => setOptionalDescription(description);
+    const handleChange = (key, value) => {
+        setState(prevState => ({ ...prevState, [key]: value }));
+    };
+
+    const dateChangeHandler = (date) => handleChange('selectedDate', date);
+    const textChangeHandler = (text) => handleChange('description', text);
+    const selectedStudentsHandler = (students) => handleChange('students', students);
+    const descriptionHandler = (description) => handleChange('optionalDescription', description);
+
+    const subjectChangeHandler = (subjectId, subjectName) => {
+        handleChange('selectedSubjectId', subjectId);
+        handleChange('selectedSubjectName', subjectName);
+
+        fetch(`/api/teacherSubjects/getStudentsByTeacherSubjectId/${subjectId}`)
+            .then(response => response.json())
+            .then(data => handleChange('allStudents', data))
+            .catch(error => console.error('Error fetching data:', error));
+    };
 
     const addingHandler = (e) => {
         e.preventDefault();
-
-        let isValid = true;
-        let newAlertMessage = "";
-
-        if (selectedDate === "") {
-            newAlertMessage = "Dátum megadása kötelező!";
-            isValid = false;
-        } else if (description === "") {
-            newAlertMessage = "Leírás megadása kötelező!";
-            isValid = false;
-        } else if (students.length === 0) {
-            newAlertMessage = "Legalább egy diák megadása kötelező!";
-            isValid = false;
-        } else if (["Exam", "MissingEquipment", "Homework"].includes(props.type) && selectedSubjectName === "") {
-            newAlertMessage = "Tantárgy megadása kötelező!";
-            isValid = false;
-        }
-
-        if (!isValid) {
-            setAlertMessage(newAlertMessage);
+        const validationMessage = validateForm(state, props.type);
+        if (validationMessage) {
+            handleChange('alertMessage', validationMessage);
             return;
         }
 
-        const formattedDate = new Date(selectedDate).toISOString();
-
+        const formattedDate = new Date(state.selectedDate).toISOString();
         const notificationData = {
             type: props.type,
             teacherId: props.teacherId,
             teacherName: props.teacherName,
-            date: formattedDate,
-            subject: selectedSubjectName,
-            description: description,
-            studentIds: students,
+            date: new Date().toISOString(),
+            dueDate: formattedDate,
+            subject: state.selectedSubjectName,
+            description: state.description,
+            studentIds: state.students,
             read: false,
-            optionalDescription: optionalDescription,
+            optionalDescription: state.optionalDescription,
         };
 
         fetch('/api/notifications/add', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(notificationData)
+            body: JSON.stringify(notificationData),
         })
             .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+                if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
                 return response.json();
             })
             .then(data => {
                 console.log('Grade added:', data);
+                props.onSuccessfulAdding();
             })
             .catch(error => console.error('Error adding grade:', error));
     };
 
-    const subjectChangeHandler = (subjectId, subjectName) => {
-        setSelectedSubjectId(subjectId);
-        setSelectedSubjectName(subjectName);
-        fetch(`/api/teacherSubjects/getStudentsByTeacherSubjectId/${subjectId}`)
-            .then(response => response.json())
-            .then(data => {
-                setAllStudents(data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    };
-
     return (
-        <Box sx={{ padding: 2 }}>
+        <Box sx={{ padding: 2, width: '100%', maxWidth: '900px', margin: '0 auto' }}>
             <Stack spacing={2} direction="column">
                 <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
-                    <DateSelector selectedDate={selectedDate} onDateChange={dateChangeHandler} />
-                    <SubjectSelector teacherSubjects={props.teacherSubjects} selectedSubjectId ={selectedSubjectId} onSubjectChange={subjectChangeHandler} />
+                    <DateSelector
+                        selectedDate={state.selectedDate}
+                        onDateChange={dateChangeHandler}
+                    />
+                    <SubjectSelector
+                        teacherSubjects={props.teacherSubjects}
+                        selectedSubjectId={state.selectedSubjectId}
+                        onSubjectChange={subjectChangeHandler}
+                    />
                 </Box>
-                <DescriptionSelector type={props.type} onDescriptionChange={textChangeHandler} />
-                <ChooseFromStudentsSelector students={allStudents} onStudentChange={selectedStudentsHandler} />
-                {props.type !== "Other" && <OptionalDescriptionSelector onOptionalDescriptionChange={descriptionHandler} />}
+
+                <DescriptionSelector
+                    type={props.type}
+                    onDescriptionChange={textChangeHandler}
+                />
+
+                <ChooseFromStudentsSelector
+                    students={state.allStudents}
+                    onStudentChange={selectedStudentsHandler}
+                />
+
+                {props.type !== "Other" && (
+                    <OptionalDescriptionSelector
+                        onOptionalDescriptionChange={descriptionHandler}
+                    />
+                )}
+
                 <Button
                     variant="contained"
-                    color="primary"
-                    sx={{ marginTop: 2 }}
+                    sx={{
+                        backgroundColor: '#82b2b8',
+                        color: '#fff',
+                        '&:hover': {
+                            backgroundColor: '#6e9ea4',
+                        },
+                    }}
                     onClick={addingHandler}
                 >
-                    {props.type === "Homework" ? "Házi feladat hozzáadása" :
-                        props.type === "Other" ? "Egyéb értesítés hozzáadása" :
-                            props.type === "Exam" ? "Dolgozat-értesítés hozzáadása" :
-                                props.type === "MissingEquipment" ? "Felszerelés-hiány hozzáadása" : "Hozzáadás"}
+                    <ButtonText type={props.type} />
                 </Button>
-                {alertMessage && (
+
+                {state.alertMessage && (
                     <Alert severity="error" sx={{ marginTop: 2 }}>
-                        {alertMessage}
+                        {state.alertMessage}
                     </Alert>
                 )}
             </Stack>
