b4a3fd4d5893f6c6ae10584717391c496352f2a5
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-26 20:58:30 +0200

    🩹 fix: routes corrected in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,9 @@ import TeacherAdding from "./admin/TeacherAdding.jsx";
 import StudentAdding from "./admin/StudentAdding.jsx";
 import Classes from "./admin/Classes.jsx";
 import ClassAdding from "./admin/ClassAdding.jsx";
-import Teacher from "./admin/Teacher.jsx";
+import TeacherMain from "./admin/TeacherMain.jsx";
+import ParentNotificationsMain from "./components/Parent/ParentNotificationsMain.jsx";
+import ParentGrades from "./components/Parent/ParentGrades.jsx";
 
 
 
@@ -25,13 +27,16 @@ createRoot(document.getElementById('root')).render(
                 <Route path='/parent/:id' element={<ParentStarter />} />
                 <Route path='/admin' element={<AdminStarter/>}/>
                 <Route path='/teacher' element={<TeacherStarter/>}/>
-                <Route path='/students' element={<Students/>}/>
-                <Route path='/teachers' element={<Teachers/>}/>
+                <Route path='/admin/students' element={<Students/>}/>
+                <Route path='/admin/teachers' element={<Teachers/>}/>
                 <Route path='/add-teacher' element={<TeacherAdding/>}/>
                 <Route path='/add-student' element={<StudentAdding/>}/>
-                <Route path='/classes' element={<Classes/>}/>
+                <Route path='/admin/classes' element={<Classes/>}/>
                 <Route path='/add-class' element={<ClassAdding/>}/>
-                <Route path='/teachers/:id' element={<Teacher />} />
+                <Route path='/admin/teachers/:id' element={<TeacherMain />} />
+                <Route path='/parent/notifications/:id' element={<ParentNotificationsMain />} />
+                <Route path='/parent/grades/:id' element={<ParentGrades />} />
+        
                
 
 
