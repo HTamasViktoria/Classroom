ab6330887fca80fe6ac503cb97dc0ed6f999c417
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-09-26 21:19:42 +0200

    🩹 fix: style-related modifications in NotificationSelector.jsx

--- client/src/components/Teacher/NotificationSelector.jsx
+++ client/src/components/Teacher/NotificationSelector.jsx
@@ -1,33 +1,69 @@
 import React, { useState } from 'react';
-import { FormControl, InputLabel, Select, MenuItem, Button } from "@mui/material";
+import { FormControl, InputLabel, Select, MenuItem, Button, Box } from "@mui/material";
 
 function NotificationSelector(props) {
     const [selectedValue, setSelectedValue] = useState('');
 
-   
     const handleChange = (event) => {
         const newValue = event.target.value;
         setSelectedValue(newValue);
-       
     };
 
+    const goBackHandler = () => {
+        props.onGoBack();
+    }
+
     return (
-        <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Értesítés típusa</InputLabel>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={selectedValue}
-                label="Értesítés típusa"
-                onChange={handleChange}
+        <Box display="flex" alignItems="center" width="100%">
+            <FormControl sx={{ minWidth: 200, mr: 2 }}>
+                <InputLabel id="demo-simple-select-label">Értesítés típusa</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={selectedValue}
+                    label="Értesítés típusa"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={"Homework"}>Házi feladat</MenuItem>
+                    <MenuItem value={"Exam"}>Dolgozat</MenuItem>
+                    <MenuItem value={"MissingEquipment"}>Felszereléshiány</MenuItem>
+                    <MenuItem value={"OtherNotification"}>Egyéb</MenuItem>
+                </Select>
+            </FormControl>
+
+            <Button
+                variant="contained"
+                sx={{
+                    flexGrow: 1,
+                    backgroundColor: '#82b2b8',
+                    color: '#fff',
+                    '&:hover': {
+                        backgroundColor: '#6e9ea4',
+                    },
+                }}
+                onClick={() => props.onChosenType(selectedValue)}
             >
-                <MenuItem value={"Homework"}>Házi feladat</MenuItem>
-                <MenuItem value={"Exam"}>Dolgozat</MenuItem>
-                <MenuItem value={"MissingEquipment"}>Felszereléshiány</MenuItem>
-                <MenuItem value={"OtherNotification"}>Egyéb</MenuItem>
-            </Select>
-            <Button onClick={()=>props.onChosenType(selectedValue)}>Típus kiválasztása</Button>
-        </FormControl>
+                Típus kiválasztása
+            </Button>
+
+          
+            <Box sx={{ mx: 1 }} />
+
+            <Button
+                variant="contained"
+                sx={{
+                    flexGrow: 1,
+                    backgroundColor: '#a2c4c6',
+                    color: '#fff',
+                    '&:hover': {
+                        backgroundColor: '#8ab2b5 ',
+                    },
+                }}
+                onClick={goBackHandler}
+            >
+                Vissza
+            </Button>
+        </Box>
     );
 }
 
