da19a7c22efb939aa6419810ddac57f2bc8b9e5e
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-02 10:46:19 +0200

    :art: Refactor code to use theme.js and styledcomponents.js

--- client/src/admin/Students.jsx
+++ client/src/admin/Students.jsx
@@ -1,54 +1,42 @@
 import { useNavigate } from "react-router-dom";
-import {useEffect, useState} from "react";
-import Button from '@mui/material/Button';
+import { useEffect, useState } from "react";
 import AdminStudentList from "../components/Admin/AdminStudentList.jsx";
 import AdminNavbar from "../components/Admin/AdminNavbar.jsx";
-import AdminTeacherList from "../components/Admin/AdminTeacherList.jsx";
+import { StyledButton } from "../../StyledComponents";
 
-function Students(){
-
+function Students() {
     const navigate = useNavigate();
 
     const [allStudents, setAllStudents] = useState([]);
 
     useEffect(() => {
-
         fetch('/api/students')
             .then(response => response.json())
             .then(data => setAllStudents(data))
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-
-    const goBackHandler =()=>{
-        navigate("/admin")
+    const goBackHandler = () => {
+        navigate("/admin");
     }
-    
-    return(<>
-        <AdminNavbar/>
-        <Button
-            variant="contained"
-            sx={{
-                backgroundColor: '#82b2b8',
-                color: '#fff',
-                '&:hover': {
-                    backgroundColor: '#6e9ea4',
-                },
-            }}
-            onClick={() => navigate("/add-student")}
-        >
-            Diák hozzáadása
-        </Button>
 
-        <AdminStudentList students={allStudents}/>
-        <Button variant="contained"
-                sx={{
-                    backgroundColor: '#bacfb0',
-                    color: '#fff',
-                    '&:hover': {
-                        backgroundColor: '#a8bfa1',
-                    },
-                }} onClick={goBackHandler}>Vissza</Button></>)
+    return (
+        <>
+            <AdminNavbar />
+            <StyledButton
+                onClick={() => navigate("/add-student")}
+            >
+                Diák hozzáadása
+            </StyledButton>
+
+            <AdminStudentList students={allStudents} />
+            <StyledButton
+                onClick={goBackHandler}
+            >
+                Vissza
+            </StyledButton>
+        </>
+    );
 }
 
-export default Students
+export default Students;
