78db9bf5c7c8e9f764bf0418b727feacc3f875ad
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-07 08:28:30 +0200

    ♻️ refactor: Starter.jsx (parent) refactored

--- client/src/parent/Starter.jsx
+++ client/src/parent/Starter.jsx
@@ -1,15 +1,12 @@
-// src/components/Starter.js
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ParentNavbar from "../components/Parent/ParentNavbar";
-import ParentNotifications from "../components/Parent/ParentNotifications";
-import ParentGrades from "../components/Parent/ParentGrades";
 import ParentMain from "../components/Parent/ParentMain";
 
 function Starter() {
     const { id } = useParams();
     const [student, setStudent] = useState(null);
-    const [notifications, setNotifications] = useState([]);
     const [refreshNeeded, setRefreshNeeded] = useState(false);
     const [lastNotifications, setLastNotifications] = useState([]);
 
@@ -21,15 +18,7 @@ function Starter() {
             })
             .catch(error => console.error('Error fetching data:', error));
     }, [id]);
-
-    useEffect(() => {
-        fetch(`/api/notifications/byStudentId/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                setNotifications(data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, [id, refreshNeeded]);
+    
 
     useEffect(() => {
         fetch(`/api/notifications/lastsByStudentId/${id}`)
@@ -50,7 +39,7 @@ function Starter() {
 
     return (
         <>
-            <ParentNavbar studentId={student.id} notifications={notifications} />
+            <ParentNavbar studentId={student.id} refreshNeeded={refreshNeeded} />
             <ParentMain lastNotifications={lastNotifications} studentId={student.id} onRefreshNeeded={refreshHandler} />
         </>
     );
