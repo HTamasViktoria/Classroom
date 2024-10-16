d489d88c1af637fc5e10b403051a14d3c8dd5e01
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-16 11:50:26 +0200

    🐛 fix: ParentStatistics.jsx corrected

--- client/src/components/Parent/ParentStatistics.jsx
+++ client/src/components/Parent/ParentStatistics.jsx
@@ -2,13 +2,23 @@ import {StatisticsContainer} from "../../../StyledComponents.js";
 import {getAverageBySubjectFullYear, getDifference} from "../../../GradeCalculations.js";
 import ClassAverageCalculator from "./ClassAverageCalculator.jsx";
 
-function ParentStatistics({grades, subject, chosenMonthIndex, id}){
-    return (<>
-        <StatisticsContainer>
-            <span>{`éves átlag: ${getAverageBySubjectFullYear(grades, subject)}`}</span>
-            <span>{`előző hónaphoz képest: ${getDifference(grades, subject, chosenMonthIndex)}`}</span>
-            <ClassAverageCalculator id={id} subject={subject}/>
-        </StatisticsContainer></>)
+function ParentStatistics({grades, subject, chosenMonthIndex, id, averages}){
+
+    
+    
+    
+    return (
+        <>
+        <span style={{ display: 'block' }}>
+            {`éves átlag: ${getAverageBySubjectFullYear(grades, subject)}`}
+        </span>
+            <span style={{ display: 'block' }}>
+            {`előző hónaphoz képest: ${getDifference(grades, subject, chosenMonthIndex)}`}
+        </span>
+            <ClassAverageCalculator averages={averages} subject={subject} />
+        </>
+    );
+
 }
 
 export default ParentStatistics
