cb354076b57b01e88f9969ab7c12b34073803e26
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-17 11:52:47 +0200

    🚧 chore: ClassroomContext.cs and Program.cs updated

--- Classroom/Data/ClassroomContext.cs
+++ Classroom/Data/ClassroomContext.cs
@@ -1,62 +1,57 @@
-using Classroom.Model.DataModels;
-using Classroom.Model.DataModels.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Classroom.Model.DataModels;
 
-namespace Classroom.Data;
-
-public class ClassroomContext : DbContext
+namespace Classroom.Data
 {
-    public DbSet<Teacher> Teachers { get; set; }
-    public DbSet<Student> Students { get; set; }
-    public DbSet<ClassOfStudents> ClassesOfStudents{get;set;}
-    public DbSet<TeacherSubject> TeacherSubjects { get; set; }
-
-    public DbSet<Grade> Grades { get; set; }   
-    public DbSet<NotificationBase> Notifications { get; set; }
-  
-    
-    public ClassroomContext(DbContextOptions<ClassroomContext> options)
-        : base(options)
+    public class ClassroomContext : IdentityDbContext<IdentityUser>
     {
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<ClassOfStudents> ClassesOfStudents { get; set; }
+        public DbSet<TeacherSubject> TeacherSubjects { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<NotificationBase> Notifications { get; set; }
+
+        public ClassroomContext(DbContextOptions<ClassroomContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>()
+                .HasMany(s => s.Grades)
+                .WithOne()
+                .HasForeignKey("StudentId")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Student>()
+                .HasMany(s => s.Notifications)
+                .WithMany(n => n.Students)
+                .UsingEntity(j => j.ToTable("StudentNotifications"));
+
+            modelBuilder.Entity<ClassOfStudents>()
+                .HasMany(c => c.Students)
+                .WithOne()
+                .HasForeignKey("ClassOfStudentsId")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TeacherSubject>()
+                .HasOne(ts => ts.ClassOfStudents)
+                .WithMany()
+                .HasForeignKey(ts => ts.ClassOfStudentsId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TeacherSubject>()
+                .HasOne(ts => ts.Teacher)
+                .WithMany()
+                .HasForeignKey(ts => ts.TeacherId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
-    
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Student>()
-            .HasMany(s => s.Grades)
-            .WithOne()
-            .HasForeignKey("StudentId")
-            .OnDelete(DeleteBehavior.Restrict);
-
-        
-        modelBuilder.Entity<Student>()
-            .HasMany(s => s.Notifications)
-            .WithMany(n => n.Students)
-            .UsingEntity(j => j.ToTable("StudentNotifications"));
-
-     
-        modelBuilder.Entity<ClassOfStudents>()
-            .HasMany(c => c.Students)
-            .WithOne()
-            .HasForeignKey("ClassOfStudentsId")
-            .OnDelete(DeleteBehavior.Restrict);
-
-    
-        modelBuilder.Entity<TeacherSubject>()
-            .HasOne(ts => ts.ClassOfStudents)
-            .WithMany()
-            .HasForeignKey(ts => ts.ClassOfStudentsId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-       
-        modelBuilder.Entity<TeacherSubject>()
-            .HasOne(ts => ts.Teacher)
-            .WithMany()
-            .HasForeignKey(ts => ts.TeacherId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        
-    }
-   
 }
--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -1,28 +1,30 @@
 using System.Text;
-//using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Classroom.Data;
 using Classroom.Service;
+using Classroom.Service.Authentication;
 using Classroom.Service.Repositories;
-using Microsoft.OpenApi.Models;
 
 public partial class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Configure services
+        
         ConfigureServices(builder.Services, builder.Configuration);
         ConfigureSwagger(builder.Services);
         ConfigureDatabaseContexts(builder.Services, builder.Configuration);
         ConfigureCustomServices(builder.Services);
+        ConfigureAuthentication(builder.Services, builder.Configuration);
 
         var app = builder.Build();
 
-        // Configure middleware
         ConfigureMiddleware(app);
-        
+
         app.Run();
     }
 
@@ -37,13 +39,36 @@ public partial class Program
         services.AddSwaggerGen(option =>
         {
             option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+            });
         });
     }
 
     private static void ConfigureDatabaseContexts(IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("MSSQL_CONNECTION");
-        
+
         services.AddDbContext<ClassroomContext>(options =>
         {
             options.UseSqlServer(connectionString);
@@ -59,6 +84,55 @@ public partial class Program
         services.AddScoped<INotificationService, NotificationService>();
         services.AddScoped<IClassOfStudentsRepository, ClassOfStudentsRepository>();
         services.AddScoped<ITeacherSubjectRepository, TeacherSubjectRepository>();
+
+        
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IAuthService, AuthService>();
+    }
+
+    private static void ConfigureAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettingsSection = configuration.GetSection("JwtSettings");
+        var validIssuer = jwtSettingsSection["ValidIssuer"];
+        var validAudience = jwtSettingsSection["ValidAudience"];
+        var issuerSigningKey = configuration["JwtSettings:IssuerSigningKey"];
+
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<ClassroomContext>()
+        .AddDefaultTokenProviders();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ClockSkew = TimeSpan.Zero,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = validIssuer,
+                    ValidAudience = validAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["access_token"];
+                        return Task.CompletedTask;
+                    },
+                };
+            });
     }
 
     private static void ConfigureMiddleware(WebApplication app)
@@ -69,13 +143,17 @@ public partial class Program
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo API V1");
-                c.RoutePrefix = string.Empty; // Swagger UI elérhető a gyökér URL-en
+                c.RoutePrefix = string.Empty;
             });
             app.UseDeveloperExceptionPage();
         }
 
         app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthentication();
         app.UseAuthorization();
+
         app.MapControllers();
     }
 }
