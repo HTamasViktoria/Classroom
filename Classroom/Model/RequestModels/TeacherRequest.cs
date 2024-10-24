d6cabb8df73ce337a6386be4647b4f19ab08d853
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-24 14:04:46 +0200

    🚧 chore: TeacherRequest.cs updated

--- Classroom/Model/RequestModels/TeacherRequest.cs
+++ Classroom/Model/RequestModels/TeacherRequest.cs
@@ -2,7 +2,11 @@ namespace Classroom.Model.RequestModels;
 
 public class TeacherRequest
 {
-    public string FamilyName { get; set; }
     public string FirstName { get; set; }
+    public string FamilyName { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+   
 
 }
