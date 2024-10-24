f6da5a26b3fbe25a9a02f8152b392418b725c904
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-24 13:53:39 +0200

    🚧 chore: new endoints added to AuthController.cs

--- Classroom/Controllers/AuthController.cs
+++ Classroom/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Classroom.Contracts;
+using Classroom.Model.DataModels;
+using Classroom.Model.RequestModels;
 using Classroom.Model.ResponseModels;
 using Classroom.Service;
 using Classroom.Service.Authentication;
@@ -22,15 +24,27 @@ namespace Classroom.Controllers
             _userService = userService;
         }
 
-        [HttpPost("sign-up")]
-        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        [HttpPost("sign-up/student")]
+        public async Task<ActionResult<RegistrationResponse>> Register(StudentRequest request)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password, (_configuration.GetSection("AppRoles:UserRoleName").Value));
+            var result = await _authenticationService.RegisterAsync(
+                request.Email,
+                request.Username,
+                request.Password,
+                role:"Student",
+                request.FirstName,
+                request.FamilyName,
+                DateTime.Parse(request.BirthDate),
+                request.BirthPlace,
+                request.StudentNo,
+                childName: null,
+                studentId:null
+            );
 
             if (!result.Success)
             {
@@ -40,12 +54,85 @@ namespace Classroom.Controllers
 
             return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
         }
-
         
+        
+        [HttpPost("sign-up/teacher")]
+        public async Task<ActionResult<RegistrationResponse>> Register(TeacherRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authenticationService.RegisterAsync(
+                request.Email,
+                request.Username,
+                request.Password,
+                role:"Teacher",
+                request.FirstName,
+                request.FamilyName,
+                null,
+                null,
+                null,
+                childName: null,
+                studentId:null
+            );
+
+            if (!result.Success)
+            {
+                AddErrors(result);
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+        }
+        
+        
+
+        [HttpPost("sign-up/parent")]
+        public async Task<ActionResult<RegistrationResponse>> Register(ParentRequest request)
+        { 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var studentIdToBe = _userService.GetIdByName(request.ChildName);
+            if (studentIdToBe == null)
+            {
+         
+              
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authenticationService.RegisterAsync(
+                request.Email,
+                request.Username,
+                request.Password,
+                role: "Parent",
+                firstName: request.FirstName,
+                familyName: request.FamilyName,
+                birthDate: null,
+                birthPlace: null,
+                studentNo: null,
+                childName: request.ChildName,
+                studentId: studentIdToBe
+            );
+
+            if (!result.Success)
+            {
+               
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+        }
+
+  
+
         [HttpPost("sign-in")]
         public async Task<ActionResult<UserResponse>> Authenticate([FromBody] AuthRequest request)
         {
-            Console.WriteLine($"Received AuthRequest: Email={request.Email}, Password={request.Password}");
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -59,17 +146,47 @@ namespace Classroom.Controllers
                 return BadRequest(ModelState);
             }
 
+   
+
+            if (result.Email == _configuration["AdminUser:Email"])
+            {
+                var admin = new Admin
+                {
+                    UserName = result.Email,
+                    Email = result.Email,
+                    Role = "Admin"
+                };
+      
+
+                return Ok(admin);
+            }
+
+           
             var userResp = _userService.GetByEmail(result.Email);
 
+            if (userResp != null)
+            {
+                userResp.Role = result.Role;
+            }
+
+           
             HttpContext.Response.Cookies.Append("access_token", result.Token, new CookieOptions { HttpOnly = true, Expires = DateTime.Now.AddMinutes(30) });
 
-            return Ok(userResp);
+            return Ok(userResp); 
         }
 
+
+
+        
+
+        
+      
+        
         
         [HttpPost("sign-out")]
         public IActionResult Logout()
         {
+            
             HttpContext.Response.Cookies.Delete("access_token");
             return Ok();
         }
@@ -81,5 +198,6 @@ namespace Classroom.Controllers
                 ModelState.AddModelError(error.Key, error.Value);
             }
         }
+
     }
 }
