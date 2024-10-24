5e818bd7fe2a128ac5ea0580348d69cb2e38e902
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-24 14:16:27 +0200

    chore: Starter.jsx updated

--- client/src/teacher/Starter.jsx
+++ client/src/teacher/Starter.jsx
@@ -1,64 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import TeacherNavbar from "../components/Teacher/TeacherNavbar.jsx";
-import { useEffect, useState } from "react";
-import { Container, MenuItem } from '@mui/material';
 import Tasks from "../components/Teacher/Tasks.jsx";
-import { StyledSelect, StyledFormControl, StyledInputLabel, StyledTypography } from '../../StyledComponents';
+
 
 function Starter() {
-    const [teachers, setTeachers] = useState([]);
-    const [selectedTeacherId, setSelectedTeacherId] = useState('');
+    const { id } = useParams();
     const [selectedTeacher, setSelectedTeacher] = useState(null);
+    
 
     useEffect(() => {
-        fetch('/api/teachers')
-            .then(response => response.json())
+        fetch(`/api/teachers/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => {
-                setTeachers(data);
+                console.log(data);
+                setSelectedTeacher(data);
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, []);
-
-    useEffect(() => {
-        const teacher = teachers.find(teacher => teacher.id === selectedTeacherId);
-        setSelectedTeacher(teacher);
-    }, [selectedTeacherId, teachers]);
-
-    const handleChange = (event) => {
-        setSelectedTeacherId(event.target.value);
-    };
+    }, [id]);
 
     return (
         <>
-            <TeacherNavbar teacher={selectedTeacher} />
-            <Container>
-                <StyledTypography variant="h4" gutterBottom>
-                    {selectedTeacher ? '' : 'Én vagyok:'}
-                </StyledTypography>
-                {selectedTeacher && (
-                    <Tasks teacherId={selectedTeacherId} teacherName={`${selectedTeacher.familyName} ${selectedTeacher.firstName}`} />
-                )}
-                {!selectedTeacher && (
-                    <StyledFormControl variant="outlined">
-                        <StyledInputLabel id="teacher-select-label">Tanár kiválasztása:</StyledInputLabel>
-                        <StyledSelect
-                            labelId="teacher-select-label"
-                            value={selectedTeacherId}
-                            onChange={handleChange}
-                            label="Tanár kiválasztása:"
-                        >
-                            {teachers.length > 0 ? (
-                                teachers.map((teacher) => (
-                                    <MenuItem key={teacher.id} value={teacher.id}>
-                                        {teacher.familyName} {teacher.firstName}
-                                    </MenuItem>
-                                ))
-                            ) : (
-                                <MenuItem disabled>No teachers available</MenuItem>
-                            )}
-                        </StyledSelect>
-                    </StyledFormControl>
-                )}
-            </Container>
+            {selectedTeacher &&  ( 
+                <>
+                    <TeacherNavbar />
+                    <Tasks teacherId={id} teacherName={`${selectedTeacher.familyName} ${selectedTeacher.firstName}`} />
+                </>
+            )}
         </>
     );
 }
