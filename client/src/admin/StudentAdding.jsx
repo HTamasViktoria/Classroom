c5bc14a320ef869aa3655ed8632463ff67c9322b
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-10-24 14:11:54 +0200

    🐛 fix: StudentAdding.jsx 's fetch endpoing updated

--- client/src/admin/StudentAdding.jsx
+++ client/src/admin/StudentAdding.jsx
@@ -9,7 +9,7 @@ function StudentAdding() {
 
     const postStudent = (student) => {
         console.log('Posting student data:', JSON.stringify(student, null, 2));
-        return fetch(`/api/students/add`, {
+        return fetch(`/api/auth/sign-up/student`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
