ac3dd1d209586bc700de823c8cb4c6d238e548be
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-13 09:34:02 +0100

    🚧 chore: NotificationRepository.cs response model class added to the backend

---
+++ Classroom/Model/ResponseModels/NotificationResponse.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Classroom.Model.DataModels;
+
+namespace Classroom.Model.ResponseModels;
+
+public class NotificationResponse
+{
+    public int Id { get; set; }
+    public string TeacherId { get; set; }
+    public string TeacherName { get; set; }
+    public string Type { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+        
+  
+    [ForeignKey("Student")]
+    public string StudentId { get; set; }
+    public Student Student { get; set; }
+        
+    public string Description { get; set; }
+    public string? SubjectName { get; set; }
+    public bool Read { get; set; }
+    public bool OfficiallyRead { get; set; }
+    public string? OptionalDescription { get; set; }
+}
