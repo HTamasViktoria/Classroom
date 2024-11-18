cc41dcec811f4df89d396fa7b0a3c22891ad2f2f
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-18 08:23:31 +0100

    ♻️ refactor: BulkGradeAddingByPerson.jsx and BulkGradeStudents.jsx refactored, BulkGradeAdding.jsx added to the teacher side

--- client/src/components/Teacher/BulkGradeAddingByPerson.jsx
+++ client/src/components/Teacher/BulkGradeAddingByPerson.jsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
-import { Box, Container, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
-import {StyledButton} from "../../../StyledComponents.js";
+import {useState } from "react";
+import { BulkContainer, StyledBox } from "../../../StyledComponents.js";
+import GradeValueSelector from "./GradeValueSelector.jsx";
+import { AButton } from "../../../StyledComponents.js";
 
-function BulkGradeAddingByPerson({selectedDate, selectedSubjectName, teacherId, selectedForWhat, student}) {
-    const [grades, setGrades] = useState([]);
+function BulkGradeAddingByPerson({ selectedDate, selectedSubjectName, teacherId, selectedForWhat, student }) {
     const [selectedGrade, setSelectedGrade] = useState("");
     const [successfullyAdded, setSuccessfullyAdded] = useState(false);
-    
-    useEffect(() => {
-        fetch('/api/grades/gradeValues')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Hiba történt az adatok betöltésekor.');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(`selectedSubjectName a bulkgradeaddingbyperson-ban: ${selectedSubjectName}`)
-                setGrades(data)
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-
-    const handleGradeChange = (e) => {
-        setSelectedGrade(e.target.value);
-    };
-
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -34,14 +13,13 @@ function BulkGradeAddingByPerson({selectedDate, selectedSubjectName, teacherId, 
         const formattedDate = new Date(selectedDate).toISOString();
 
         const gradeData = {
-            teacherId: teacherId.toString(),
-            studentId: student.id.toString(),
+            teacherId: teacherId,
+            studentId: student.id,
             subject: selectedSubjectName,
             forWhat: selectedForWhat,
             value: selectedGrade,
             date: formattedDate
         };
-        console.log(gradeData)
 
         fetch('/api/grades/add', {
             method: 'POST',
@@ -61,68 +39,29 @@ function BulkGradeAddingByPerson({selectedDate, selectedSubjectName, teacherId, 
             .catch(error => console.error('Error adding grade:', error));
     };
 
-
     const formatGrade = (grade) => {
         const [stringGrade, numberGrade] = grade.split(' = ');
         return `${stringGrade}(${numberGrade})`;
     };
 
+    const gradeChangeHandler = (e) => setSelectedGrade(e.target.value);
 
     return (
         <>
             {successfullyAdded ? (
                 <div>{`Jegy sikeresen hozzáadva: ${formatGrade(selectedGrade)}`}</div>
-
             ) : (
-                <Container>
-                    <Box my={4}>
-                    <Typography variant="h6" gutterBottom>
-                            Jegy kiválasztása
-                        </Typography>
+                <BulkContainer>
+                    <StyledBox>
                         <form onSubmit={handleSubmit}>
-                            <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
-                                <InputLabel id="grade-select-label">Jegy kiválasztása</InputLabel>
-                                <Select
-                                    labelId="grade-select-label"
-                                    value={selectedGrade}
-                                    onChange={handleGradeChange}
-                                    label="Jegy kiválasztása"
-                                    sx={{
-                                        backgroundColor: 'background.default',
-                                        color: 'text.primary',
-                                        '& .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: 'primary.main',
-                                        },
-                                        '&:hover .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: 'primary.dark',
-                                        },
-                                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: 'primary.dark',
-                                        },
-                                    }}
-                                >
-                                    {grades.length > 0 ? (
-                                        grades.map((grade) => (
-                                            <MenuItem key={grade.id || grade} value={grade}>
-                                                {grade}
-                                            </MenuItem>
-                                        ))
-                                    ) : (
-                                        <MenuItem disabled>No grades available</MenuItem>
-                                    )}
-                                </Select>
-                                <StyledButton type='submit'>
-                                    Jegy hozzáadása
-                                </StyledButton>
-                            </FormControl>
+                            <GradeValueSelector selectedGrade={selectedGrade} handleGradeChange={gradeChangeHandler} />
+                            <AButton type="submit">Jegy hozzáadása</AButton>
                         </form>
-                    </Box>
-                </Container>
+                    </StyledBox>
+                </BulkContainer>
             )}
         </>
     );
-
-
 }
 
 export default BulkGradeAddingByPerson;
--- client/src/components/Teacher/BulkGradeStudents.jsx
+++ client/src/components/Teacher/BulkGradeStudents.jsx
@@ -1,47 +1,37 @@
-import { useEffect, useState } from "react";
-import { CustomBox, StyledTypography, StyledButton } from '../../../StyledComponents';
+import { useState } from "react";
+import { CustomBox, StyledTypography, AButton, BulkStudentsList, ListItem} from '../../../StyledComponents';
 import BulkGradeAddingByPerson from "./BulkGradeAddingByPerson.jsx";
-function BulkGradeStudents({ teacherSubject, teacherId, selectedDate, selectedForWhat, selectedSubjectName }) {
+import StudentsOfATeacherSubjectFetcher from "./StudentsOfATeacherSubjectFetcher.jsx";
+function BulkGradeStudents({ selectedSubjectId, teacherId, selectedDate, selectedForWhat, selectedSubjectName }) {
     const [students, setStudents] = useState([]);
     
-
-    useEffect(() => {
-        fetch(`/api/teacherSubjects/getStudentsByTeacherSubjectId/${teacherSubject}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Hiba történt a diákok betöltésekor');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(`selectedsubjectname a bulkgradestudents-ban:${selectedSubjectName}`)
-                setStudents(data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, [teacherSubject]);
-
-
-
-    
+   
     return (
+        <>
+            <StudentsOfATeacherSubjectFetcher 
+                selectedSubjectId={selectedSubjectId} 
+                onData={(data)=>setStudents(data)}/>
+            
         <CustomBox>
             {students.length > 0 ? (
-                <ul style={{ padding: 0 }}>
+                <BulkStudentsList>
                     {students.map(student => (
-                        <li key={student.id} style={{ listStyle: 'none', marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
-                            <StyledButton style={{ marginRight: '10px' }}>
+                        <ListItem key={student.id}>
+                            <AButton>
                                 {student.familyName} {student.firstName}
-                            </StyledButton>
-                    <BulkGradeAddingByPerson teacherId={teacherId} selectedForWhat={selectedForWhat} selectedDate={selectedDate}
+                            </AButton>
+                    <BulkGradeAddingByPerson teacherId={teacherId} 
+                                             selectedForWhat={selectedForWhat} 
+                                             selectedDate={selectedDate}
                                              selectedSubjectName={selectedSubjectName}
                                              student={student} />
-                        </li>
+                        </ListItem>
                     ))}
-                </ul>
+                </BulkStudentsList>
             ) : (
                 <StyledTypography>Nincsenek diákok a kiválasztott tantárgyban</StyledTypography>
             )}
-        </CustomBox>
+        </CustomBox></>
     );
 }
 
