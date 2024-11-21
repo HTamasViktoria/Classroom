78e9c44cc6d3a21d3dc68a7fae384b55afc6a75b
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-21 20:53:56 +0100

    🚧 chore: SentMessages.jsx and SentFetcher.jsx added to the frontend, to the common components

---
+++ client/src/common/components/SentMessages.jsx
@@ -0,0 +1,34 @@
+import SentFetcher from "./SentFetcher.jsx";
+import {useState} from "react";
+import {IncomingMessagesContainer, MessageDetail, MessageLine} from "../../../StyledComponents.js";
+import formatDate from "./formatDate.js";
+function SentMessages({id}){
+    
+    const [sentMessages, setSentMessages] = useState([])
+    
+    
+    return(<>
+    <SentFetcher id={id} onData={(data)=>setSentMessages(data)}/>
+        <IncomingMessagesContainer>
+            {sentMessages.length === 0  &&(<h1>Nincsenek elküldött üzenetek</h1>)}
+            {sentMessages.map((message) => (
+                <MessageLine key={message.id} onClick={()=>onChosing(message.id) }>
+                    {message.read === true && (<button>Olvasatlannak jelöl</button>)}
+                    <MessageDetail>{formatDate(message.date)}</MessageDetail>
+                    <MessageDetail>{message.senderName}</MessageDetail>
+                    <MessageDetail>{message.headText}</MessageDetail>
+                    <MessageDetail>
+                        {message.text.length > 20 ? message.text.substring(0, 20) + "..." : message.text}
+                    </MessageDetail>
+                    <MessageDetail>
+                        <button>Törlés</button>
+                    </MessageDetail>
+
+                </MessageLine>
+            ))}
+
+        </IncomingMessagesContainer>
+    </>)
+}
+
+export default SentMessages
