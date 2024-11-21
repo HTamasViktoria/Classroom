50854d71c945b40f1fb9efcc386a74bdf45b15f4
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-21 20:37:46 +0100

    🚧 chore: ReceiverResponse.cs added to the response model classes

---
+++ Classroom/Model/ResponseModels/ReceiverResponse.cs
@@ -0,0 +1,8 @@
+namespace Classroom.Model.ResponseModels;
+
+public class ReceiverResponse
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Role { get; set; }
+}
