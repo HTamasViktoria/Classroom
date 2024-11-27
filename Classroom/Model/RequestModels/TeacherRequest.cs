6dc23551aec349487ed5c45c0fb9fd5456baaaa2
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-27 11:23:51 +0100

    🚧 chore: Required attribute added to StudentRequest.cs and TeacherRequest.cs request model classes

--- Classroom/Model/RequestModels/TeacherRequest.cs
+++ Classroom/Model/RequestModels/TeacherRequest.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Classroom.Model.RequestModels;
 
 public class TeacherRequest
 {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string FamilyName { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
    
 
