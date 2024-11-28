55a5a7a0d178d7acbda6a10c23fbde9ddfebed4f
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-28 09:43:23 +0100

    🚧 chore: NotificationRequest.cs got required attributes

--- Classroom/Model/RequestModels/NotificationRequest.cs
+++ Classroom/Model/RequestModels/NotificationRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Classroom.Model.DataModels;
 using Classroom.Model.DataModels.Enums;
 
@@ -5,17 +6,15 @@ namespace Classroom.Model.RequestModels;
 
 public class NotificationRequest
 {
-   
-        public string Type { get; set; }
-        public string TeacherId { get; set; }
-        public string TeacherName { get; set; }
-        public DateTime Date { get; set; }
-        public DateTime DueDate { get; set; }
-        public string? Subject { get; set; }
-        public bool Read { get; set; }
-        public bool OfficiallyRead { get; set; }
-        public string Description { get; set; }
-        public List<string> StudentIds { get; set; }
-        public string? OptionalDescription { get; set; }
-    
+    [Required] public string Type { get; set; }
+    [Required] public string TeacherId { get; set; }
+    [Required] public string TeacherName { get; set; }
+    [Required] public DateTime Date { get; set; }
+    [Required] public DateTime DueDate { get; set; }
+    public string? Subject { get; set; }
+    [Required] public bool Read { get; set; }
+    [Required] public bool OfficiallyRead { get; set; }
+    [Required] public string Description { get; set; }
+    [Required] public List<string> StudentIds { get; set; }
+    public string? OptionalDescription { get; set; }
 }
