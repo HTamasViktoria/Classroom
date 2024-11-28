da2c19b88099c1ec36248f2825e2e40679684c27
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-11-28 09:47:43 +0100

    🚧 chore: MessageRequest.cs got required attributes

--- Classroom/Model/RequestModels/MessageRequest.cs
+++ Classroom/Model/RequestModels/MessageRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Classroom.Model.DataModels;
 
 namespace Classroom.Model.RequestModels;
@@ -5,9 +6,13 @@ namespace Classroom.Model.RequestModels;
 public class MessageRequest
 {
     public int Id { get; set; }
+    [Required]
     public DateTime Date { get; set; }
+    [Required]
     public string FromId { get; set; }
+    [Required]
     public List<string> ReceiverIds { get; set; }
     public string HeadText { get; set; }
+    [Required]
     public string Text { get; set; }
 }
