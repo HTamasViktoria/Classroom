c8871bd17d83f0bea66ecbe31164041e942019a8
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-12-03 13:26:32 +0100

    🚧 chore: fetch function in Students.jsx now sending also the token

--- client/src/admin/Students.jsx
+++ client/src/admin/Students.jsx
@@ -4,34 +4,47 @@ import AdminStudentList from "../components/Admin/AdminStudentList.jsx";
 import AdminNavbar from "../components/Admin/AdminNavbar.jsx";
 import { AButton } from "../../StyledComponents";
 
+
 function Students() {
     const navigate = useNavigate();
-
     const [allStudents, setAllStudents] = useState([]);
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    
 
+    
+    
     useEffect(() => {
-        fetch('/api/students')
-            .then(response => response.json())
-            .then(data => setAllStudents(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
+        const fetchStudents = async () => {
+            try {
+                const response = await fetch('/api/students', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setAllStudents(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
- 
+        fetchStudents();
+    }, [token]);
 
     return (
         <>
             <AdminNavbar />
-            <AButton
-                onClick={() => navigate("/add-student")}
-            >
+            <AButton onClick={() => navigate("/add-student")}>
                 Diák hozzáadása
             </AButton>
-      
 
             <AdminStudentList students={allStudents} />
-            <AButton
-                onClick={()=>navigate("/admin")}
-            >
+            <AButton onClick={() => navigate("/admin")}>
                 Vissza
             </AButton>
         </>
