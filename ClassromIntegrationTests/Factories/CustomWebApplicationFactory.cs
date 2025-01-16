44417576407d1246989e275f492c6ee955ed2a8d
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2025-01-16 11:49:16 +0100

    🐛 bug: WebApplicationFactory.cs fixed to use properly the in-memory db.

--- ClassromIntegrationTests/Factories/CustomWebApplicationFactory.cs
+++ ClassromIntegrationTests/Factories/CustomWebApplicationFactory.cs
@@ -8,30 +8,35 @@ using Microsoft.AspNetCore.Hosting;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    public void ConfigureTestServices(IServiceCollection services)
-    {
-        var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ClassroomContext>));
-        if (dbContextDescriptor != null)
-        {
-            services.Remove(dbContextDescriptor);
-        }
-
-        services.AddDbContext<ClassroomContext>(options =>
-            options.UseInMemoryDatabase("TestDatabase"));
-        
-        var serviceProvider = services.BuildServiceProvider();
-        using (var scope = serviceProvider.CreateScope())
-        {
-            var context = scope.ServiceProvider.GetRequiredService<ClassroomContext>();
-            context.Database.EnsureCreated();
-        }
-    }
-    
+    private readonly string _dbName = Guid.NewGuid().ToString();
+   
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            ConfigureTestServices(services);
+            // Keressük meg és távolítsuk el a meglévő ClassroomContext konfigurációját
+            var classroomDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ClassroomContext>));
+            if (classroomDescriptor != null)
+            {
+                services.Remove(classroomDescriptor);
+            }
+            
+            // Hozzáadjuk a ClassroomContext-et egy in-memory adatbázissal
+            services.AddDbContext<ClassroomContext>(options =>
+            {
+                options.UseInMemoryDatabase(_dbName);
+            });
+            
+            // Adatbázis inicializálása
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var classroomContext = scope.ServiceProvider.GetRequiredService<ClassroomContext>();
+            
+            // Adatbázis törlése és létrehozása
+            classroomContext.Database.EnsureDeleted();
+            classroomContext.Database.EnsureCreated();
+            
+            // Ide jöhet további inicializálás (pl. alapadatok betöltése)
+            // classroomContext.SeedData();
         });
     }
 }
